54c4387dd8124a99539a5bcdc25440213e532604
Rameshwarrathi <rameshwarrathi01@gmail.com>
2021-09-17 12:15:50 +0530

    End Of Section 13

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
+import { take } from 'rxjs/operators';
 import { Member } from 'src/app/_models/member';
+import { Pagination } from 'src/app/_models/pagination';
+import { User } from 'src/app/_models/user';
+import { UserParams } from 'src/app/_models/userParams';
+import { AccountService } from 'src/app/_services/account.service';
 import { MembersService } from 'src/app/_services/members.service';
 //import { MembersService } from 'src/app/_services/members.service';
 
@@ -10,20 +15,49 @@ import { MembersService } from 'src/app/_services/members.service';
   styleUrls: ['./member-list.component.css']
 })
 export class MemberListComponent implements OnInit {
-  members$:Observable<Member[]>;
+  //members$:Observable<Member[]>;
+  members :Member[];
+  pagination: Pagination;
+  //pageNumber =1;
+  //pageSize=5;
+  userParams:UserParams;
+  user:User;
+  genderList= [{value:'male', display:'Males'},{value:'female', display:'Females'}];
 
 
-  constructor(private memberservice: MembersService) { }
+  constructor(private memberservice: MembersService) { 
+    //,private accountService:AccountService
+    // this.accountService.currentUser$.pipe(take(1)).subscribe(user => {
+    //   this.user =user;
+    //   this.userParams=new UserParams(user);
+    // })
 
-  ngOnInit(): void {
-    //this.loadMembers();
-    this.members$ =this.memberservice.getMembers();
+    this.userParams= this.memberservice.getUserParams();
   }
 
-  /*loadMembers() {
-    this.memberservice.getMembers().subscribe(members => {
-      this.members =members;
+  ngOnInit(): void {
+    this.loadMembers();
+    //this.members$ =this.memberservice.getMembers();
+  }
+
+  loadMembers() {
+    this.memberservice.setUserParams(this.userParams);
+    this.memberservice.getMembers(this.userParams).subscribe(response => {
+      this.members =response.result;
+      this.pagination = response.pagination;
     })
-  }*/
+  }
+
+  pageChanged(event:any) {
+    this.userParams.pageNumber= event.page;
+    this.memberservice.setUserParams(this.userParams);
+    this.loadMembers();
+  }
+
+  resetFilters(){
+    //this.userParams =new UserParams(this.user);
+    this.userParams =this.memberservice.resetUserParams();
+    this.loadMembers();
+  }
 
 }
